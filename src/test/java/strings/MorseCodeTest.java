406bde56dfcd067a9721df03b1be6c8603078faf
Robert Lehmann <Robert.Lehmann@protonmail.com>
2025-05-18 13:01:24 +0200

    add more unit tests using complex strings

--- src/test/java/strings/MorseCodeTest.java
+++ src/test/java/strings/MorseCodeTest.java
@@ -23,10 +23,27 @@ class MorseCodeTest {
     }
 
     @ParameterizedTest
+    @CsvFileSource(resources = "/testdata_get_cleartxt_of_strings.csv", nullValues = "null")
+    void getClearTextOfStrings(String key, String value) {
+        if(key == null) {
+            assertThrows(IllegalArgumentException.class, () -> new MorseCode(key));
+            return;
+        }
+        MorseCode mc = new MorseCode(key);
+        assertEquals(value, mc.getClearText());
+    }
+
+    @ParameterizedTest
     @CsvSource(ignoreLeadingAndTrailingWhitespace = false,  value = {"null,''", "'\n',''", "\r,''", "\t,''", "A,''"},
                                                             nullValues = "null")
     @CsvFileSource(resources = "/testdata_decode_sequence_of_single_letters.csv")
     void decodeMorseSequenceOfSingleLetters(String key, String value) {
+        assertEquals(value, MorseCode.decode(key));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testdata_decode_sequence_of_strings.csv", nullValues = "null")
+    void decodeMorseSequenceOfStrings(String key, String value) {
         assertEquals(value, MorseCode.decode(key));
     }
 
@@ -38,6 +55,11 @@ class MorseCodeTest {
         assertEquals(value, MorseCode.decode(key));
     }
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testdata_decode_signal_sequence_of_strings.csv", nullValues = "null")
+    void decodeSignalSequenceOfStrings(String key, String value) {
+        assertEquals(value, MorseCode.decode(key));
+    }
 
     @ParameterizedTest
     @CsvSource(ignoreLeadingAndTrailingWhitespace = false, value = {"'\n', ", "\r, ", "\t,         "})
@@ -48,6 +70,17 @@ class MorseCodeTest {
     }
 
     @ParameterizedTest
+    @CsvFileSource(resources = "/testdata_get_sequence_of_strings.csv", nullValues = "null")
+    void getMorseSequenceOfStrings(String key, String value) {
+        if(key == null) {
+            assertThrows(IllegalArgumentException.class, () -> new MorseCode(key));
+            return;
+        }
+        MorseCode mc = new MorseCode(key);
+        assertEquals(value, mc.getNotation());
+    }
+
+    @ParameterizedTest
     @CsvSource(ignoreLeadingAndTrailingWhitespace = false, value = {"'\n', ", "\r, ", "\t,˽˽˽˽˽˽˽˽˽˽˽˽˽˽˽˽˽˽˽˽˽"})
     @CsvFileSource(resources = "/testdata_get_signal_sequence_of_single_chars.csv")
     void getSignalSequenceOfSingleChars(String key, String value) {
@@ -55,8 +88,22 @@ class MorseCodeTest {
         assertEquals(key.matches("[\r\n]") ? System.lineSeparator() : value, mc.getNotation(true));
     }
 
-    @Test
-    void getStackedNotation() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testdata_get_signal_sequence_of_strings.csv", nullValues = "null")
+    void getSignalSequenceOfStrings(String key, String value) {
+        if(key == null) {
+            assertThrows(IllegalArgumentException.class, () -> new MorseCode(key));
+            return;
+        }
+        MorseCode mc = new MorseCode(key);
+        assertEquals(value, mc.getNotation(true));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testdata_get_stacked_notation_of_strings.csv")
+    void getStackedNotation(String key, String value) {
+        MorseCode mc = new MorseCode(key);
+        assertEquals(value, mc.getStackedNotation());
     }
 
     @ParameterizedTest
@@ -65,5 +112,12 @@ class MorseCodeTest {
     void getSpokenNotationOfSingleChars(String key, String value) {
         MorseCode mc = new MorseCode(key);
         assertEquals(key.matches("[\r\n]") ? System.lineSeparator() : value, mc.getSpokenNotation());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testdata_get_spoken_notation_of_strings.csv")
+    void getSpokenNotationOfStrings(String key, String value) {
+        MorseCode mc = new MorseCode(key);
+        assertEquals(value, mc.getSpokenNotation());
     }
 }
